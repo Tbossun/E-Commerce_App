5ba797a37144c2fce2c5bdc22e866a61961a60fd
Sodiq Alabi <alabisdq@gmail.com>
2023-06-18 13:35:32 +0100

    initial commit

---
+++ E-Commerce_Mvc/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_Mvc.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ E-Commerce_Mvc/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce_Mvc.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        public string DisplayOrder { get; set; }
+    }
+}
